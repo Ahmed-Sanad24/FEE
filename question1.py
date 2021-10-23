9d620cf8fb383f5e3099047dad4d44474d9673bd
Ahmed-Sanad24 <a.sanad24@yahoo.com>
2021-10-23 07:03:29 +0200

    the first question

---
+++ question1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def add(a, b):
+    "Return the sum of a and b"
+    print("Passed a = %s and b = %s, returning a + b = %s" % (a, b, a + b))
+    return a + b
+
+
+# In[3]:
+
+
+test1=add(5,10)
+
+
+# In[4]:
+
+
+test2=add(7,3)
+
+
+# In[ ]:
+
+
+
+
